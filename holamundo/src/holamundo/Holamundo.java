637d5f4f120ff7a872efc69208597aeaa7ab3a55
mi.defuentes <mi.defuentes@106-07.info2>
2024-09-20 14:14:49 +0200

    Prueba Hola mundo

---
+++ holamundo/src/holamundo/Holamundo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package holamundo;
+
+/**
+ * 
+ */
+public class Holamundo 
+{
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hola Mundo. It´s me, Israel De Fuentes");
+
+	}
+
+}
